53a85654cbc5adbb7cfe0abc4aa7ee051c9c06c2
kstefaine <kstefanie@gmail.com>
2013-10-04 00:11:54 -0700

    Example of how to use a module

--- express-app/app.js
+++ express-app/app.js
@@ -5,6 +5,11 @@ var express = require('express');
 // Create an instance of Express
 var app = express();
 
+// Load the iniparser module
+var iniparser = require('iniparser');
+// Read the ini file and populate the content on the config object
+var config = iniparser.parseSync('./config.ini');
+
 // Set the view engine
 app.set('view engine', 'jade');
 // Where to find the view files
@@ -12,10 +17,16 @@ app.set('views', './views');
 
 // Mark the public dir as a static dir
 app.use(express.static('./public'));
+// Add the responseTime middleware
+app.use(express.responseTime());
+// Expliciyly add the router middleware
+app.use(app.router);
+// Add the errorHandler middleware
+app.use(express.errorHandler());
 
 // A route for the home page - will render a view
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {title:config.title, message:config.message});
 });
 
 // A route for /say-hello - will render a view
@@ -25,7 +36,12 @@ app.get('/say-hello', function(req, res){
 
 app.get('/test', function(req, res){
 	res.send('this is a test');
-})
+});
+
+app.get('/fail', function(req, res){
+	fail();
+});
+
 //start the app
 http.createServer(app).listen(3000, function() {
 	console.log('App started');
